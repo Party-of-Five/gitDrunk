8b639d404d2dcbc9a3e2678792a32ce26dd456cc
Ashley Lerma <lerma.scout@gmail.com>
2020-05-28 23:36:11 -0500

    can search by drink name and console log the resulting array. pseudocode added for next steps

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,29 @@
 $("#cocktailSubBtn").click(function (event) {
-	event.preventDefault();
-	cocktailInput = $("#cocktailInput").val().trim();
-	searchCocktail(cocktailInput);
+  event.preventDefault();
+  cocktailInput = $("#cocktailInput").val().trim();
+  searchCocktail(cocktailInput);
 });
 
 function searchCocktail(cocktailVal) {
-	var settings = {
-		url: `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktailVal}`,
-		method: "GET",
-		timeout: 0,
-		headers: {},
-	};
+  var settings = {
+    url: `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktailVal}`,
+    method: "GET",
+    timeout: 0,
+    headers: {},
+  };
 
-	$.ajax(settings).done(function (response) {
-		console.log(response);
-	});
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+    let allDrinks = response.drinks;
+    // loop through the api response
+    // create button for each drink in array
+    // name the button correctly
+    // id/value equal to spot in array
+    // append to #searchResults
+    for (let i = 0; i < allDrinks.length; i++) {
+      console.log(allDrinks[i].strDrink);
+      let drinkName = allDrinks[i].strDrink;
+      let drinkBtn = $("<button>");
+    }
+  });
 }
-
-// $("#citySubBtn").click(function (event) {
-// 	event.preventDefault();
-// 	cityInput = $("#cityInput").val().trim();
-// 	cities.push(cityInput);
-// 	// Store current City Value in History
-// 	localStorage.setItem("cities", JSON.stringify(cities));
-// 	// Call the APIs
-// 	getCityInfo(cityInput);
-// });
