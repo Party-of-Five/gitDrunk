5c9b044bdc57b69f91ff63dc0ed1de43e40701f7
juanlajara <juanlajara001@gmail.com>
2020-05-28 22:44:31 -0500

    + ajax & jquery link plus API call

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,27 @@
-var settings = {
-    "url": "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita",
-    "method": "GET",
-    "timeout": 0,
-    "headers": {
-      "Cookie": "__cfduid=d3e813ce88e955b4387b21279e07cd83d1590720521"
-    },
-  };
-  
-  $.ajax(settings).done(function (response) {
-    console.log(response);
-  });
+$("#cocktailSubBtn").click(function (event) {
+	event.preventDefault();
+	searchCocktail();
+});
+
+function searchCocktail() {
+	var settings = {
+		url: "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita",
+		method: "GET",
+		timeout: 0,
+		headers: {},
+	};
+
+	$.ajax(settings).done(function (response) {
+		console.log(response);
+	});
+}
+
+// $("#citySubBtn").click(function (event) {
+// 	event.preventDefault();
+// 	cityInput = $("#cityInput").val().trim();
+// 	cities.push(cityInput);
+// 	// Store current City Value in History
+// 	localStorage.setItem("cities", JSON.stringify(cities));
+// 	// Call the APIs
+// 	getCityInfo(cityInput);
+// });
