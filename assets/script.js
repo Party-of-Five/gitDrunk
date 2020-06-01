e2b2e774949d71583d2aeb79292a5d4f251640cc
juanlajara <juanlajara001@gmail.com>
2020-06-01 17:47:51 -0500

    + Multiple Ingredient Search and Updated API keys

--- assets/script.js
+++ assets/script.js
@@ -10,75 +10,74 @@
 
 // When you click the search button
 $("#cocktailSubBtn").click(function (event) {
-  event.preventDefault();
-  // Input taken and assigned to variable
-  cocktailInput = $("#cocktailInput").val().trim();
-  // Call search cocktail function
-  searchCocktail(cocktailInput);
+	event.preventDefault();
+	// Input taken and assigned to variable
+	cocktailInput = $("#cocktailInput").val().trim();
+	// Call search cocktail function
+	searchCocktail(cocktailInput);
 });
 
 // Uses the input to search the API
 function searchCocktail(cocktailVal) {
-  var settings = {
-    url: `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktailVal}`,
-    method: "GET",
-    timeout: 0,
-    headers: {},
-  };
+	var settings = {
+		url: `https://www.thecocktaildb.com/api/json/v2/9973533/search.php?s=${cocktailVal}`,
+		method: "GET",
+		timeout: 0,
+		headers: {},
+	};
 
-  $.ajax(settings).done(function (response) {
-    console.log(response);
-    let allDrinks = response.drinks;
-    // Creates a button for each search result
-    for (let i = 0; i < allDrinks.length; i++) {
-      console.log(allDrinks[i].strDrink);
-      let drinkName = allDrinks[i].strDrink;
-      $("#searchResults").append(
-        `<li><button id="drink${i}" type="submit">${drinkName}</button></li>`
-      );
-    }
-  });
+	$.ajax(settings).done(function (response) {
+		console.log(response);
+		let allDrinks = response.drinks;
+		// Creates a button for each search result
+		for (let i = 0; i < allDrinks.length; i++) {
+			console.log(allDrinks[i].strDrink);
+			let drinkName = allDrinks[i].strDrink;
+			$("#searchResults").append(
+				`<li><button id="drink${i}" type="submit">${drinkName}</button></li>`
+			);
+		}
+	});
 }
 
 // JS FOR SEARCH BY INGREDIENT PAGE
 
 $(".ingredientAddBtn").click(function () {
-  let value = $(".ingredientInfo").val().trim();
-  $(".listIng").append("<li>" + value + "</li>");
+	let value = $(".ingredientInfo").val().trim();
+	$(".listIng").append("<li>" + value + "</li>");
 });
 
 $(".ingredientSubBtn").click(function () {
-  event.preventDefault();
-  value = $(".ingredientInfo").val().trim();
-  console.log(value);
-  // find out how to pull from multiple ingredients at once.  redo search to use listed ingredients as criteria.
-  searchIngredient(value);
+	event.preventDefault();
+	value = $(".ingredientInfo").val().trim();
+	console.log(value);
+	// find out how to pull from multiple ingredients at once.  redo search to use listed ingredients as criteria.
+	searchIngredient(value);
 });
 
 function searchIngredient(ingredient) {
-  var settings = {
-    async: true,
-    crossDomain: true,
-    url: "https://the-cocktail-db.p.rapidapi.com/filter.php?i=" + ingredient,
-    method: "GET",
-    headers: {
-      "x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
-      "x-rapidapi-key": "fec2323914msh6be937a2ff5cba0p1cc78ejsn762f18f5e051",
-    },
-  };
-  $.ajax(settings).done(function (response) {
-    console.log(response);
+	var settings = {
+		async: true,
+		crossDomain: true,
+		url:
+			"https://www.thecocktaildb.com/api/json/v2/9973533/filter.php?i=" +
+			ingredient,
+		method: "GET",
+		headers: {},
+	};
+	$.ajax(settings).done(function (response) {
+		console.log(response);
 
-    let allDrinks = response.drinks;
-    // Creates a button for each search result
-    for (let i = 0; i < allDrinks.length; i++) {
-      console.log(allDrinks[i].strDrink);
-      let drinkName = allDrinks[i].strDrink;
-      $(".ingResults").append(
-        `<li><button id="identifyDrink drink${i}" type="submit">${drinkName}</button></li>`
-      );
-    }
-  });
+		let allDrinks = response.drinks;
+		// Creates a button for each search result
+		for (let i = 0; i < allDrinks.length; i++) {
+			console.log(allDrinks[i].strDrink);
+			let drinkName = allDrinks[i].strDrink;
+			$(".ingResults").append(
+				`<li><button id="identifyDrink drink${i}" type="submit">${drinkName}</button></li>`
+			);
+		}
+	});
 }
 
 // JS FOR ROULETTE PAGE
@@ -88,34 +87,33 @@ function searchIngredient(ingredient) {
 
 // Random drink generator based on liquor button clicked
 $(".liquorSelect").click(function () {
-  event.preventDefault();
-  // WHY?? Every liquor button is showing as vodka
-  let liquor = $(".liquorSelect").attr("data-name");
-  console.log(liquor);
+	event.preventDefault();
+	// WHY?? Every liquor button is showing as vodka
+	let liquor = $(".liquorSelect").attr("data-name");
+	console.log(liquor);
 
-  getCocktail(liquor);
+	getCocktail(liquor);
 });
 
 function getCocktail(liquor) {
-  var settings = {
-    async: true,
-    crossDomain: true,
-    url: "https://the-cocktail-db.p.rapidapi.com/filter.php?i=" + liquor,
-    method: "GET",
-    headers: {
-      "x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
-      "x-rapidapi-key": "fec2323914msh6be937a2ff5cba0p1cc78ejsn762f18f5e051",
-    },
-  };
+	var settings = {
+		async: true,
+		crossDomain: true,
+		url:
+			"https://www.thecocktaildb.com/api/json/v2/9973533/filter.php?i=" +
+			liquor,
+		method: "GET",
+		headers: {},
+	};
 
-  $.ajax(settings).done(function (response) {
-    let possibleDrinks = response.drinks;
-    // Randomly chooses a cocktail from the array of drinks containing that liquor
-    var randomCocktail =
-      possibleDrinks[Math.floor(Math.random() * possibleDrinks.length)];
-    // Console log random selection
-    console.log(randomCocktail.strDrink);
-  });
+	$.ajax(settings).done(function (response) {
+		let possibleDrinks = response.drinks;
+		// Randomly chooses a cocktail from the array of drinks containing that liquor
+		var randomCocktail =
+			possibleDrinks[Math.floor(Math.random() * possibleDrinks.length)];
+		// Console log random selection
+		console.log(randomCocktail.strDrink);
+	});
 }
 
 // create function for #identifyDrink to pull up information by drink id and display it in .featureText and .featureImage
